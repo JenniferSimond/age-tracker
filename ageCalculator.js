4cbcf0a49976ee994477635ea24ac18b2824c47d
Jennifer Simond <jenniasimond@gmail.com>
2024-01-12 11:06:20 -0500

    Updated Picture, naming convention, and comments

--- ageCalculator.js
+++ ageCalculator.js
@@ -1,45 +1,54 @@
+// Main video resource for project ^Resource_1: https://youtu.be/_pw8vk1tAhs?si=b0gVQRBP09ddLVHl
+
+/* Func calaculates the number of days in a month for a given year.
+    Date constructor() ^Resource_2: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/Date */
 function getDaysInMonth(year, month) {
+    /*the 0 is where we set day parameter to 0 so return last day of previous month. to get 
+    the num of days in the given month.*/
     return new Date(year, month, 0).getDate();
 }
-
+// Func calculates age based on user input & current date.
+  // "+1" added to month becuase JavaScript is a count from zero language, so Jan has an index of.
 function calculate() {
     let userInput = document.getElementById("date");
     let birthDate = new Date(userInput.value);
 
-    let date1 = birthDate.getDate();
-    let month1 = birthDate.getMonth() + 1;
-    let year1 = birthDate.getFullYear();
+    let birthDay = birthDate.getDate();
+    let birthMonth = birthDate.getMonth() + 1;
+    let birthYear = birthDate.getFullYear();
 
+    /* the "new" keyword is used in JavaScript to create an object from a constructor function. 
+    The "new" keyword has to be placed before the constructor func call ^Resource_3: https://stackoverflow.com/questions/1646698/what-is-the-new-keyword-in-javascript#:~:text=The%20new%20keyword%20is%20used,the%20constructor%20function's%20prototype%20property */
     let today = new Date();
-    let date2 = today.getDate();
-    let month2 = today.getMonth() + 1;
-    let year2 = today.getFullYear();
+    let currentDay = today.getDate();
+    let currentMonth = today.getMonth() + 1;
+    let currentYear = today.getFullYear();
 
-    let currentYear = year2 - year1;
-    let currentMonth;
-    let currentDate;
+    let yearDifference = currentYear - birthYear;
+    let monthDifference;
+    let dayDifference;
 
-    if (month2 >= month1) {
-        currentMonth = month2 - month1;
+    if (currentMonth >= birthMonth) {
+        monthDifference = currentMonth - birthMonth;
     } else {
-        currentYear--;
-        currentMonth = 12 + month2 - month1;
+        yearDifference--;
+        monthDifference = 12 + currentMonth - birthMonth;
     }
 
-    if (date2 >= date1) {
-        currentDate = date2 - date1;
+    if (currentDay >= birthDay) {
+        dayDifference = currentDay - birthDay;
     } else {
-        currentMonth--;
-        currentDate = getDaysInMonth(year1, month1) + date2 - date1;
+        monthDifference--;
+        dayDifference = getDaysInMonth(birthYear, birthMonth) + currentDay - birthDay;
     }
 
-    if (currentMonth < 0) {
-        currentMonth = 11;
-        currentYear--;
+    if (monthDifference < 0) {
+        monthDifference = 11;
+        yearDifference--;
     }
 
     // Output or process the age data
-    console.log(`Age: ${currentYear} years, ${currentMonth} months, and ${currentDate} days.`);
+    console.log(`Age: ${yearDifference} years, ${monthDifference} months, and ${dayDifference} days.`);
 }
 
 // Setting maximum date input value to today and adding event listener for the calculate button
